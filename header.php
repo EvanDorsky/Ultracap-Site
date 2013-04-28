0b11c1bda6e80038b13cfcb2231695c529db6d83
Abe Levitan <allevitan@gmail.com>
2013-04-27 23:33:01 -0400

    More testing

---
+++ header.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="polygon.css">
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!Doctype html>
 <html lang="en">
   <head>
-    <?php include 'header.php'?>
+    <?php include 'header.php'; ?>
     <title>D3 Test</title>
     <script type="text/javascript" src="newPolygon.js"></script>
   </head>
