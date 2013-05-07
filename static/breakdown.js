2fc21ba9e4dfda9fdb39c378b6498b0969532000
Abe Levitan <allevitan@gmail.com>
2013-05-07 11:39:33 -0400

    Do some stuff

--- static/breakdown.js
+++ static/breakdown.js
@@ -1,11 +1,11 @@
 function callOn(me){
     link = d3.select(me).attr('link');
     $('.infostuff').animate({
-        scrollTop: $("div[link=" + link + "]:not(.sbitem)").offset().top -
+        scrollTop: $(".infostuff [link=" + link + "]:not(.sbitem)").offset().top -
 	    $("div.infostuff").offset().top + $("div.infostuff").scrollTop()
     }, 500);
     //stuff that turns on linked polygon
-    var targetGon = d3.select('.polygon[link = "' + link + '"]');
+    var targetGon = d3.select('.polygon[link="' + link + '"]');
     //the targeted polygon
 
     targetGon.transition()
